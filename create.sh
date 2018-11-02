3a8a82ac0f410aae43bef56b9a6df31a234b8755
jtkDvlp <jtkdevelopments@gmail.com>
2018-11-02 23:03:14 +0100

    Fixes bugs, adds uninstall and adds outputs

--- create.sh
+++ create.sh
@@ -1,45 +1,46 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-serviceTemplate="`cat service.template`"
-logrotateTemplate="`cat logrotate.template`"
-
-if [ $(id -u) -ne 0 ]; then 
+if [ $(id -u) -ne 0 ]; then
   echo "Error: Please run as root"
   exit 1
 fi
 
-if [ -z ${name+x} ] || [ -z ${command+x} ]; then
-	echo "Error: Please set name and command parameter"
+if [ -z ${name+x} ] || [ -z ${cmd+x} ]; then
+	echo "Error: Please set name and cmd parameter"
+  echo "usage: cat create.sh | name=\"x\" cmd=\"y\" [user dir desc deps (escape \$ with \\)]"
 	exit 1
 fi
-serviceTemplate="${serviceTemplate//<COMMAND>/$(printf %q "$command")}"
 
 if [ -f "/etc/init.d/$name" ]; then
 	echo "Error: Service '$name' already exists"
 	exit 1
 fi
+
+serviceTemplate="`cat service.template`"
+logrotateTemplate="`cat logrotate.template`"
+
+serviceTemplate="${serviceTemplate//<CMD>/$cmd}"
 serviceTemplate="${serviceTemplate//<NAME>/$name}"
+serviceTemplate="${serviceTemplate//<USER>/${user:-}}"
+serviceTemplate="${serviceTemplate//<DIR>/${dir:-}}"
+serviceTemplate="${serviceTemplate//<DESC>/${desc:-Starting $name}}"
+serviceTemplate="${serviceTemplate//<DEPS>/${deps:-\$remote_fs \$syslog}}"
+
 logrotateTemplate="${logrotateTemplate//<NAME>/$name}"
 
-if ! id -u "$username" &> /dev/null; then
-	echo "Error: User '$username' not found"
-	exit 1
-fi
-serviceTemplate="${serviceTemplate//<USER>/$user}"
-
-serviceTemplate="${serviceTemplate//<DIRECTORY>/${directory:-.}}"
-serviceTemplate="${serviceTemplate//<DESCRIPTION>/${description:-Starts '$name'-Service}}"
-serviceTemplate="${serviceTemplate//<REQUIRED-START>/${required-start:-\$remote_fs \$syslog}}"
-serviceTemplate="${serviceTemplate//<REQUIRED-STOP>/${required-stop:-\$remote_fs \$syslog}}"
-
-#install
+echo "...creating service in /etc/init.d"
 echo -e "$serviceTemplate" > "/etc/init.d/$name"
 chmod +x "/etc/init.d/$name"
+echo "...creating log files in /var/log"
 touch "/var/log/$name.log"
 chown "$user" "/var/log/$name.log"
 touch "/var/log/$name.err"
 chown "$user" "/var/log/$name.err"
+echo "...adding logrotate configuration"
 echo -e "$logrotateTemplate" > "/etc/logrotate.d/$name"
+echo "...adding service for startup at boot"
 update-rc.d "$name" defaults
+echo "finished"
+
 service "$name" start
